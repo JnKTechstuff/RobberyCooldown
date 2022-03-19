55f1d530a483319af911e6911ea4a40addcc0200
JnKTechstuff <7122547+JnKTechstuff@users.noreply.github.com>
2022-03-19 08:29:57 -0400

    Update fxmanifest.lua

--- RobberyCooldown/fxmanifest.lua
+++ RobberyCooldown/fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
-
 game 'gta5'
+
+author 'JnKTechstuff <JnKTechstuff#2217>'
+description 'A robbery queuing and event firing system to prevent your cops from wanting to die!'
+version '1.0.0'
 
 server_scripts {
 	'config/config.lua',
